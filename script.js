7c2de79f522796ff108b6bd83c3efd4ed58ae981
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-01-10 16:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+           const input = (year) => {
+            if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+                alert("366")
+            }
+            else{
+                alert("365");
+            }
+           }
+    input(prompt(""));
